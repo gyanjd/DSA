ddb15c8f502a2d1fde08f47bb6a3e9c8d61f9621
Gyan Jyoti Das <gyanjd@gmail.com>
2023-10-05 12:08:00 +0530

    add recursive memoize fib solution

---
+++ recursiveMemoizeFibonacci.cpp
@@ -0,0 +1,21 @@
+// recursive solution 
+// time - 2^n space - O(n)
+
+int getNthFib(int n) {
+  if(n==1) return 0;
+  else if(n==2) return 1;
+  else return getNthFib(n-1)+getNthFib(n-2);
+}
+
+___________________________________________________________________
+
+#include<unordered_map>
+
+unordered_map<int,int> um = {{1,0},{2,1}};
+int getNthFib(int n) {
+  if(um.count(n)) return um[n];
+  else {
+    um[n] = getNthFib(n-1) + getNthFib(n-2);
+    return um[n];
+  }
+}
